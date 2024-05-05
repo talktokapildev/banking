7f911a4932557eb9d92debff8ca7632141db6855
Kapil Dev <talktokapildev@gmail.com>
2024-05-05 16:24:34 +0100

    layout of pages

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,5 @@
-// import MobileNav from "@/components/MobileNav";
-// import Sidebar from "@/components/Sidebar";
+import MobileNav from "@/components/MobileNav";
+import Sidebar from "@/components/Sidebar";
 // import { getLoggedInUser } from "@/lib/actions/user.actions";
 import Image from "next/image";
 import { redirect } from "next/navigation";
@@ -10,18 +10,20 @@ export default async function RootLayout({
   children: React.ReactNode;
 }>) {
   // const loggedIn = await getLoggedInUser();
+  const loggedIn = {firstName: 'Adrian', lastName: 'JSM'};
 
   // if(!loggedIn) redirect('/sign-in')
 
   return (
     <main className="flex h-screen w-full font-inter">
-      {/* <Sidebar user={loggedIn} /> */}
-      SIDEBAR
+      <Sidebar user={loggedIn} />
+      {/* <Sidebar /> */}
+      {/* SIDEBAR */}
       <div className="flex size-full flex-col">
         <div className="root-layout">
           <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
           <div>
-            {/* <MobileNav user={loggedIn} /> */}
+            <MobileNav user={loggedIn} />
           </div>
         </div>
         {children}
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,42 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox';
+import RightSidebar from '@/components/RightSidebar';
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = {firstName: 'Adrian'};
+
+  // const accounts = await getAccounts({ 
+  //   userId: loggedIn.$id 
+  // })
+  const accounts = [{name: 'Bank 1', currentBalance:1250}, {name: 'Bank 2', currentBalance:150}, {name: 'Bank 3', currentBalance:950}];
+
+  if(!accounts) return;
+  
+  //const accountsData = accounts?.data;
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className="home-content">
+        <header className='home-header'>
+           <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+           <TotalBalanceBox 
+            accounts={[{name: 'Bank 1', currentBalance:1250}, {name: 'Bank 2', currentBalance:150}, {name: 'Bank 3', currentBalance:950}]}
+            totalBanks={1}
+            totalCurrentBalance={1250.351}
+          />
+        </header>
+      </div>
+       <RightSidebar 
+        user={loggedIn}
+        transactions={[]}
+        banks={[{}, {}]}
+      />
+    </section>
   )
 }
 
